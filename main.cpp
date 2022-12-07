c3b423c396ab5b20b00075e8f403b7d956933099
Gionanthony <117936434+Gionanthony@users.noreply.github.com>
2022-12-07 19:23:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char a;
+    cin >> a;
+    funzione (a);
+
+    if ((funzione(a) == true)) {
+        if ((a>=65) && (a<=90)) {
+            char b = a+32;
+            cout << b;
+        } else {
+            char b = a-32;
+            cout << b;
+        }
+    } else {
+        cout << "errore";
+    }
+
+    return 0;
 }
